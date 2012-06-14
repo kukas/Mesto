18f4a8846e2f53a70a48468664e33b853e5da85d
Jirka Balhar <balhar.j@gmail.com>
2012-06-14 22:55:54 +0200

    Revert "Revert "Nástin adresářové struktury + Nějaké ty libs + font""
    This reverts commit eddc4f0b821a9ddfdd262416c512c5230b700f04.

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+function Game(){
+	return;
+}
+Game.prototype.init = function() {
+	return;
+};
---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+var game;
+$(document).ready( function(){
+	// Zjištění WebGL podpory
+	if( !Detector.webgl ){
+		Detector.addGetWebGLMessage();
+		return;
+	}
+	// Zavedení objektu game
+	game = new Game();
+
+	game.init();
+} );
