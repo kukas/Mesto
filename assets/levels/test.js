c059b50e1f5001f99b75c3ca71221f3914b54c1e
Izzet <stepanmarek@centrum.cz>
2012-07-27 14:04:25 +0200

    Posílám slíbeného panáčka
    Má několik zákkladních animací, ale nějak se mu buguje bounding box.
    Running animation je nastavená schválně takto, aby byla plynulejší.

--- assets/levels/test.js
+++ assets/levels/test.js
@@ -13,6 +13,7 @@ function Level(){
 		lamp: this.modelpath+"lamp/untitled.js",
 		kostka: this.modelpath+"anim.js",
 		monster: this.modelpath+"monster.js",
+		panacek:this.modelpath+"panacek.js",
 	};
 	// důležité: pro některé funkce musí mít textury rozměry power of two, tedy 2,4,8,16,32,64, ...
 	this.textures = {
@@ -44,6 +45,21 @@ Level.prototype.afterLoad = function (){
 	}
 	
 	this.objects.push( new Environment(this.textures.steel, 0, 0, 0, 2400, 1200, false) );
+	
+	this.objects.push( new Thing(this.models.panacek,{
+			position:new THREE.Vector3(250,250,0),
+			scale:new THREE.Vector3(50,50,50),
+			animation:{
+				interpolace:20,
+				startingAnim:"standing",
+				modelAnimations:{
+					standing:[1,2],
+					running:[5,75],
+					nodding:[80,110],
+					falling:[110,190],
+				},
+			},
+	}) );
 	
 	// todo: udělat to obecný a hezký -> světlo = lampa
 	this.objects.push( new Lamp(this.models.lamp, {
